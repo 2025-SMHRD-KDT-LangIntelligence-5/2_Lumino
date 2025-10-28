981a93ef41e22777ecbccbdfe64fb87e2f2d86ce
YHHLuchas <1130yhh@Gmail.com>
2025-10-28 17:14:46 +0900

    커뮤니티 컨트롤러만들어서 기능 정의, 커뮤니티 페이지, 어카운트 노티피케이션 수정 했다가 원상복구

---
+++ src/main/java/com/smhrd/boot/controller/CommunityController.java
@@ -0,0 +1,14 @@
+package com.smhrd.boot.controller;
+
+import com.smhrd.boot.entity.CommunityPage;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CommunityController {
+
+    @GetMapping("/account-notifications")
+    public String community(CommunityPage community) {
+        return "account-notifications"; // templates 폴더의 account-notifications.html을 찾음
+    }
+}
