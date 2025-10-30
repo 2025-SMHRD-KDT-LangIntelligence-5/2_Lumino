162c21e831f5215cff5c6412bacb6b2d34dbba9f
youna-1 <dldbsk416@gmail.com>
2025-10-30 12:26:47 +0900

    í메인 페이지 추가 했습니다.

--- src/main/java/com/smhrd/boot/entity/Main.java
+++ src/main/java/com/smhrd/boot/entity/Main.java
@@ -10,7 +10,7 @@ import lombok.Setter;
 @Getter // 필드에 접근할 수 있는 메서드
 @Setter // 필드 값 수정 or 초기화 메서드
 @Entity // JPA 사용시 꼭 추가해줘야 하는 *필수* 어노테이션 -> Movie 클래스 형태로 테이블을 생성
-@Table(name="") // 생성되는 테이블의 이름 지정(생략하면 클래스 이름과 동일하게 생성)
+@Table(name="categories") // 생성되는 테이블의 이름 지정(생략하면 클래스 이름과 동일하게 생성)
 public class Main {
     @Id // 기본키
     private Integer category_id;
