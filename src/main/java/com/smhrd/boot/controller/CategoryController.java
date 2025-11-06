c708bfed90ac0b97400e7167d0111f750893fb6f
YHHLuchas <1130yhh@Gmail.com>
2025-11-06 12:10:55 +0900

    조인 로그인 페이지 서식 수정, 카테고리 페이지 8개 만들어서 추가, 카테고리 컨트롤러에 각각 매핑

--- src/main/java/com/smhrd/boot/controller/CategoryController.java
+++ src/main/java/com/smhrd/boot/controller/CategoryController.java
@@ -15,5 +15,53 @@ public class CategoryController {
         return "category_page"; // templates 폴더의 account-notifications.html을 찾음
     }
 
+    // 카테고리 페이지 매핑
+    @GetMapping("/category_all")
+    public String category_all() {
+        return "category_all"; // templates 폴더의 account-notifications.html을 찾음
+    }
+
+    // 카테고리 페이지 매핑
+    @GetMapping("/category_audio")
+    public String category_audio() {
+        return "category_audio"; // templates 폴더의 account-notifications.html을 찾음
+    }
+
+    // 카테고리 페이지 매핑
+    @GetMapping("/category_coding")
+    public String category_coding() {
+        return "category_coding"; // templates 폴더의 account-notifications.html을 찾음
+    }
+
+    // 카테고리 페이지 매핑
+    @GetMapping("/category_education")
+    public String category_education() {
+        return "category_education"; // templates 폴더의 account-notifications.html을 찾음
+    }
+
+    // 카테고리 페이지 매핑
+    @GetMapping("/category_image")
+    public String category_image() {
+        return "category_image"; // templates 폴더의 account-notifications.html을 찾음
+    }
+
+    // 카테고리 페이지 매핑
+    @GetMapping("/category_search")
+    public String category_search() {
+        return "category_search"; // templates 폴더의 account-notifications.html을 찾음
+    }
+
+    // 카테고리 페이지 매핑
+    @GetMapping("/category_text")
+    public String category_text() {
+        return "category_text"; // templates 폴더의 account-notifications.html을 찾음
+    }
+
+    // 카테고리 페이지 매핑
+    @GetMapping("/category_video")
+    public String category_video() {
+        return "category_video"; // templates 폴더의 account-notifications.html을 찾음
+    }
+
 
 }
