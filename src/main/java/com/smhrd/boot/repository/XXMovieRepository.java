52b7ee39b65a3386d0a8eb0564c27f2d1b28cce0
YHHLuchas <1130yhh@Gmail.com>
2025-11-19 09:41:50 +0900

    클로드가 자꾸 수업파일 건드려서 빌드 실패해서 지움

--- src/main/java/com/smhrd/boot/repository/XXMovieRepository.java
+++
@@ -1,9 +0,0 @@
-package com.smhrd.boot.repository;
-
-import com.smhrd.boot.entity.XXMovie;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository                                    //<테이블로 만들어지는 객체(class)의 이름, class에 지정된 @ID의 타입>
-public interface XXMovieRepository extends JpaRepository<XXMovie, Long> {
-}
